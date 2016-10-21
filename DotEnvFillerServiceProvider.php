db60c118c864ef70e78ea2d0a1c21c80da2d8cba
kduma <krystian@muzik.pl>
2016-10-21 13:11:31 +0000

    Applied fixes from StyleCI

--- DotEnvFillerServiceProvider.php
+++ DotEnvFillerServiceProvider.php
@@ -1,46 +1,44 @@
-<?php namespace KDuma\DotEnvFiller;
+<?php
+
+namespace KDuma\DotEnvFiller;
 
 use Illuminate\Support\ServiceProvider;
 
 /**
- * Class DotEnvFillerServiceProvider
- * @package KDuma\DotEnvFiller
+ * Class DotEnvFillerServiceProvider.
  */
-class DotEnvFillerServiceProvider extends ServiceProvider {
+class DotEnvFillerServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('command.config.env', function () {
+            return new DotEnvFillerCommand();
+        });
 
+        $this->commands('command.config.env');
+    }
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->singleton('command.config.env', function()
-		{
-			return new DotEnvFillerCommand();
-		});
-
-		$this->commands('command.config.env');
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array(
-			'command.config.env'
-		);
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'command.config.env',
+        ];
+    }
 }
